90369d09d0b7365b478e7b38817fba9afc02440c
Steve Mao <maochenyan@gmail.com>
2016-10-08 13:07:45 +1100

    implement threshold

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 const Color = require('color');
 const eases = require('eases');
 
-const tick = setImmediate || process.nextTick || setTimeout;
-
 function howMuchTransition(elapsed, duration, timing) {
 	// TODO: not sure why we need `1 -` here.
 	// probably related to https://github.com/Qix-/color/issues/46
@@ -19,8 +17,16 @@ module.exports = (color1, color2, opts, cb) => {
 	opts = Object.assign({
 		duration: 1000,
 		timing: 'linear',
-		threshold: null
+		threshold: 60
 	}, opts);
+
+	let tick;
+
+	if (opts.threshold) {
+		tick = setTimeout;
+	} else {
+		tick = setImmediate || process.nextTick || setTimeout;
+	}
 
 	if (typeof opts.timing === 'string') {
 		opts.timing = eases[opts.timing];
@@ -34,22 +40,37 @@ module.exports = (color1, color2, opts, cb) => {
 	color2 = new Color(color2);
 
 	let elapsed = 0;
-	const first = new Date();
+	const first = new Date().getTime();
 
-	function next(color1, color2, pre) {
-		const now = new Date();
+	function next(color1, color2, lastTime) {
+		const currTime = new Date().getTime();
+		const idealTimeToCall = 1000 / opts.threshold;
+		let timeToCall;
+
+		if (opts.threshold) {
+			timeToCall = idealTimeToCall;
+		}
 
 		// ignore the first call
-		if (pre) {
-			const delta = now - pre;
+		if (lastTime) {
+			const delta = currTime - lastTime;
 
+			if (opts.threshold) {
+				if (delta > idealTimeToCall * 2) {
+					timeToCall = 0;
+				} else {
+					timeToCall = idealTimeToCall * 2 - delta;
+				}
+			}
+
+			// ignore if next is called too quickly
 			if (delta) {
 				elapsed += delta;
 				const {timing, duration} = opts;
 				const percent = howMuchTransition(elapsed, duration, timing);
 				const color = color1.clone();
 				color.mix(color2, percent);
-				if (now.getTime() >= first.getTime() + duration) {
+				if (currTime >= first + duration) {
 					// last call, flush out color2
 					cb(color2.rgbArray(), delta);
 					return;
@@ -60,8 +81,9 @@ module.exports = (color1, color2, opts, cb) => {
 		}
 
 		tick(() => {
-			next(color1, color2, now);
-		});
+			next(color1, color2, currTime);
+			// if tick is setImmediate or process.nextTick, we ignore timeToCall
+		}, timeToCall);
 	}
 
 	next(color1, color2);
--- test.js
+++ test.js
@@ -3,17 +3,23 @@ import m from './';
 
 test.cb('linear', t => {
 	let lastR = 0;
+	let callCount = 0;
+
 	setTimeout(() => {
 		t.is(lastR, 255);
+		t.true(callCount <= 60, 'maximum call count is 60');
 		t.end();
 	}, 1500);
 
 	m('blue', 'red', color => {
-		t.true(color[0] + color[2] === 255 || color[0] + color[2] === 256 || color[0] + color[2] === 254);
-		t.true(color[0] > 0);
-		t.true(color[2] < 255);
+		console.log(color);
+		callCount++;
 		const currentR = color[0];
-		t.true(lastR <= currentR);
+
+		t.true(color[0] + color[2] === 255 || color[0] + color[2] === 256 || color[0] + color[2] === 254, 'the total value should be approx. 255');
+		t.true(color[0] > 0, 'red value should be greater than 0');
+		t.true(color[2] < 255, 'blue value should be less than 255');
+		t.true(lastR <= currentR, 'last red value should be less than current red value');
 		lastR = currentR;
 	});
 });
